83840f057e84785a9275cfa5015647e98d91ff4b
HLK9 <62252259+HLK9@users.noreply.github.com>
2021-07-04 16:37:17 +0700

    Update Program.cs

--- Kaliz/Program.cs
+++ Kaliz/Program.cs
@@ -16,9 +16,7 @@ namespace Kaliz
             
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //
-            
-            //        
+                  
             Application.Run(new Kaliz());
         }
 
