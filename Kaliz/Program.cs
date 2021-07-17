666ee9872d946efec109f5e172b24416e191dfcc
HLK9 <62252259+HLK9@users.noreply.github.com>
2021-07-17 09:49:35 +0700

    Update Program.cs

--- Kaliz/Program.cs
+++ Kaliz/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Threading;
 using System.Windows.Forms;
 
@@ -7,6 +6,7 @@ namespace Kaliz
 {
     static class Program
     {
+        public static SplashForm splashForm = null;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -16,10 +16,38 @@ namespace Kaliz
             
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-                  
-            Application.Run(new Kaliz());
+            //
+            //show splash
+            Thread splashThread = new Thread(new ThreadStart(
+                delegate
+                {
+                    splashForm = new SplashForm();
+                    Application.Run(splashForm);
+                }
+                ));
+
+            splashThread.SetApartmentState(ApartmentState.STA);
+            splashThread.Start();
+
+            //run form - time taking operation
+            //MainForm mainForm = new MainForm();
+            //mainForm.Load += new EventHandler(mainForm_Load);
+            Kaliz Ka = new Kaliz();
+            Ka.Load += Ka_Load;
+            //
+            Application.Run(Ka);
         }
 
-       
+        private static void Ka_Load(object sender, EventArgs e)
+        {
+            if (splashForm == null)
+            {
+                return;
+            }
+
+            splashForm.Invoke(new Action(splashForm.Close));
+            splashForm.Dispose();
+            splashForm = null;
+        }
     }
 }
