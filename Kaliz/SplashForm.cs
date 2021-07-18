aec9d69fdbdb8c73fc6768ce4ae6278295f6d65f
HLK9 <62252259+HLK9@users.noreply.github.com>
2021-07-18 08:05:23 +0700

    splash added

---
+++ Kaliz/SplashForm.cs
@@ -0,0 +1,20 @@
+﻿using Kaliz.Properties;
+using System.Windows.Forms;
+
+namespace Kaliz
+{
+    public partial class SplashForm : Telerik.WinControls.UI.ShapedForm
+    {
+        public SplashForm()
+        {
+            //960, 540 Cỡ form 1/2 ảnh 1080
+            InitializeComponent(); PictureBox spashPictureBox = new PictureBox();
+            spashPictureBox.Image = Resources.Ps;
+            spashPictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            spashPictureBox.Dock = DockStyle.Fill;
+            this.Controls.Add(spashPictureBox);
+
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+    }
+}
