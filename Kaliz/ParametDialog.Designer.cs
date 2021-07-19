755270c9460c773e499998ca0e46f4c2c5e8f67c
HLK9 <62252259+HLK9@users.noreply.github.com>
2021-07-19 15:28:34 +0700

    Parameter Added

---
+++ Kaliz/ParametDialog.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Kaliz
+{
+    partial class ParametDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.materialTealTheme1 = new Telerik.WinControls.Themes.MaterialTealTheme();
+            this.radTextBox1 = new Telerik.WinControls.UI.RadTextBox();
+            this.radLabel1 = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel2 = new Telerik.WinControls.UI.RadLabel();
+            this.radButton1 = new Telerik.WinControls.UI.RadButton();
+            this.radButton2 = new Telerik.WinControls.UI.RadButton();
+            this.radButton3 = new Telerik.WinControls.UI.RadButton();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radTextBox1
+            // 
+            this.radTextBox1.Location = new System.Drawing.Point(12, 89);
+            this.radTextBox1.Name = "radTextBox1";
+            this.radTextBox1.NullText = "Input";
+            this.radTextBox1.Size = new System.Drawing.Size(392, 36);
+            this.radTextBox1.TabIndex = 0;
+            this.radTextBox1.ThemeName = "MaterialTeal";
+            // 
+            // radLabel1
+            // 
+            this.radLabel1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radLabel1.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.radLabel1.Location = new System.Drawing.Point(71, 12);
+            this.radLabel1.Name = "radLabel1";
+            this.radLabel1.Size = new System.Drawing.Size(271, 33);
+            this.radLabel1.TabIndex = 1;
+            this.radLabel1.Text = "Add Parameter to Compiler";
+            // 
+            // radLabel2
+            // 
+            this.radLabel2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radLabel2.ForeColor = System.Drawing.Color.Orchid;
+            this.radLabel2.Location = new System.Drawing.Point(12, 64);
+            this.radLabel2.Name = "radLabel2";
+            this.radLabel2.Size = new System.Drawing.Size(254, 19);
+            this.radLabel2.TabIndex = 2;
+            this.radLabel2.Text = "Note: Each parameter is separated by a space";
+            // 
+            // radButton1
+            // 
+            this.radButton1.Location = new System.Drawing.Point(12, 164);
+            this.radButton1.Name = "radButton1";
+            this.radButton1.Size = new System.Drawing.Size(120, 36);
+            this.radButton1.TabIndex = 3;
+            this.radButton1.Text = "Cancel";
+            this.radButton1.ThemeName = "MaterialTeal";
+            this.radButton1.Click += new System.EventHandler(this.radButton1_Click);
+            // 
+            // radButton2
+            // 
+            this.radButton2.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.radButton2.Location = new System.Drawing.Point(284, 164);
+            this.radButton2.Name = "radButton2";
+            this.radButton2.Size = new System.Drawing.Size(120, 36);
+            this.radButton2.TabIndex = 4;
+            this.radButton2.Text = "OK";
+            this.radButton2.ThemeName = "MaterialTeal";
+            this.radButton2.Click += new System.EventHandler(this.radButton2_Click);
+            // 
+            // radButton3
+            // 
+            this.radButton3.Location = new System.Drawing.Point(148, 164);
+            this.radButton3.Name = "radButton3";
+            this.radButton3.Size = new System.Drawing.Size(120, 36);
+            this.radButton3.TabIndex = 4;
+            this.radButton3.Text = "How to?";
+            this.radButton3.ThemeName = "MaterialTeal";
+            // 
+            // ParametDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 212);
+            this.Controls.Add(this.radButton3);
+            this.Controls.Add(this.radButton2);
+            this.Controls.Add(this.radButton1);
+            this.Controls.Add(this.radLabel2);
+            this.Controls.Add(this.radLabel1);
+            this.Controls.Add(this.radTextBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ParametDialog";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.ShowIcon = false;
+            this.Text = "Parameter Dialog";
+            this.ThemeName = "MaterialTeal";
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.Themes.MaterialTealTheme materialTealTheme1;
+        private Telerik.WinControls.UI.RadTextBox radTextBox1;
+        private Telerik.WinControls.UI.RadLabel radLabel1;
+        private Telerik.WinControls.UI.RadLabel radLabel2;
+        private Telerik.WinControls.UI.RadButton radButton1;
+        private Telerik.WinControls.UI.RadButton radButton2;
+        private Telerik.WinControls.UI.RadButton radButton3;
+    }
+}
---
+++ Kaliz/ParametDialog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Kaliz
+{
+    public partial class ParametDialog : Telerik.WinControls.UI.RadForm
+    {
+        public string ParametText { get; set; }
+        public ParametDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void radButton2_Click(object sender, EventArgs e)
+        {
+            ParametText = radTextBox1.Text;
+        }
+
+        private void radButton1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
