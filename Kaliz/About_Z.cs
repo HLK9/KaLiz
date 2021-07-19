f8fb5035ef16e8c4b34af2582a2988c34615558a
HLK9 <62252259+HLK9@users.noreply.github.com>
2021-07-19 15:28:43 +0700

    Update About_Z.cs

--- Kaliz/About_Z.cs
+++ Kaliz/About_Z.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
-using System.Windows.Forms;
-using Telerik.WinControls;
+using System.Diagnostics;
+
 
 namespace Kaliz
 {
@@ -24,6 +19,11 @@ namespace Kaliz
         private void radLabel7_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void radButton1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://cmbst.wordpress.com/aboutkaliz/");
         }
     }
 }
