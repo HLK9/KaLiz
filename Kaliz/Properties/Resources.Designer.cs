f213e6e13ce9bd07578f1b21e8981b8a66332475
HLK9 <62252259+HLK9@users.noreply.github.com>
2021-08-18 21:56:38 +0700

    Update Resources.Designer.cs

--- Kaliz/Properties/Resources.Designer.cs
+++ Kaliz/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace Kaliz.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Kaliz_Beta1 {
+            get {
+                object obj = ResourceManager.GetObject("Kaliz-Beta1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Ps {
             get {
                 object obj = ResourceManager.GetObject("Ps", resourceCulture);
