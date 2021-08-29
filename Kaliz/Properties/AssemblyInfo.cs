d260e60cb3f19a9ddc7fe0057578bddfb7c655de
Hoang Long <62252259+HLK9@users.noreply.github.com>
2021-08-29 22:10:43 +0700

    up

--- Kaliz/Properties/AssemblyInfo.cs
+++ Kaliz/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Kaliz")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Phan mem lap trinh thong minh")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("CMB Studio")]
 [assembly: AssemblyProduct("Kaliz")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
