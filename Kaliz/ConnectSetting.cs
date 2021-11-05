b5e64b02edd18b6a3d9f79b650aede03a4945176
Hoang Long <62252259+HLK9@users.noreply.github.com>
2021-11-05 19:43:48 +0700

    Create ConnectSetting.cs

---
+++ Kaliz/ConnectSetting.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+
+namespace Kaliz
+{
+    public partial class ConnectSetting : Telerik.WinControls.UI.RadForm
+    {
+        public string IPAddress { get; set; }
+        public string Port { get; set; }
+        public ConnectSetting()
+        {
+            InitializeComponent();
+        }
+
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            IPAddress = txtAddress.Text;
+            Port = txtPort.Text;
+        }
+    }
+}
