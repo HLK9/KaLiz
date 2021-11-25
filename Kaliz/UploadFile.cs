27e11780ef154cb004126a6a1635a946b2e4c200
Hoang Long <62252259+HLK9@users.noreply.github.com>
2021-11-25 21:40:39 +0700

    Create UploadFile.cs

---
+++ Kaliz/UploadFile.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.IO;
+
+namespace Kaliz
+{
+    public partial class UploadFile : Telerik.WinControls.UI.RadForm
+    {
+        public UploadFile()
+        {
+            InitializeComponent();
+        }
+
+        private void radTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radButton1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Mo = new OpenFileDialog();
+            if (Mo.ShowDialog() == DialogResult.OK)
+            {
+                txtPath.Text = Mo.FileName;
+            }
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            if(File.Exists(txtPath.Text))
+            {
+                try
+                {
+                    using (WebClient client = new WebClient())
+                    {
+                        var resStr = client.UploadFile("https://file.io", txtPath.Text);
+                        var jObjResult = JObject.Parse(Encoding.UTF8.GetString(resStr));
+                        var linkToFile = jObjResult["link"];
+                        txtLink.Text = linkToFile.ToString();
+                    }
+                }
+                catch
+                { }
+            }
+           
+        }
+    }
+}
